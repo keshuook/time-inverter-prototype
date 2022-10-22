c2a99791ae1fcdba4fd3bc29243bbd98ee879c26
Keshuook <59391084+keshuook@users.noreply.github.com>
2022-10-22 13:21:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,21 +24,22 @@ public class Main {
     private static boolean inverted = false;
     private static int time = 0;
     private static int FPS = 30;
-    private static int[] positionsX = new int[0];
-    private static int[] positionsY = new int[0];
+    private static int numberOfInversions = 0;
+    private static int[][] positionsX = new int[3][];
+    private static int[][] positionsY = new int[3][];
     private static long frameTime = 0;
 
     public static void main(String args[]) {
-        p.moveBy(0, 100);
+        p.reset();
         frame = new JFrame();
-        
+
         frame.setSize(700, 550);
         frame.setLocation(200, 50);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setBackground(BG_COLOR);
         frame.setVisible(true);
-        
+
         frame.addKeyListener(new GameKeyListener(p));
         frame.requestFocus();
         while(true) {
@@ -54,29 +55,56 @@ public class Main {
         g.clearRect(0, 0, 700, 550);
         if(p.getX() <= 200 && p.getX() >= 100 && p.getY() >= 30 && p.getY() <= 80){
             inverted = inverted ? false : true;
+            int i;
 
-            positionsX = new int[time];
-            positionsY = new int[time];
-            for(int i = time-1;i >= 0;i--){
-                positionsY[i] = positions.pop();
-                positionsX[i] = positions.pop();
+            if(inverted) {
+                positionsX[numberOfInversions] = new int[time];
+                positionsY[numberOfInversions] = new int[time];
+                for(i = time-1;!positions.isEmpty();i--){
+                    positionsY[numberOfInversions][i] = positions.pop();
+                    positionsX[numberOfInversions][i] = positions.pop();
+                }
+                for(int j = i;j >= 0;j--){
+                    positionsY[numberOfInversions][j] = 2000;
+                    positionsX[numberOfInversions][j] = 2000;
+                }
+            }else{
+                int size = time + positions.size()/2;
+                positionsX[numberOfInversions] = new int[size];
+                positionsY[numberOfInversions] = new int[size];
+                for(i = time;!positions.isEmpty();i++){
+                    positionsY[numberOfInversions][i] = positions.pop();
+                    positionsX[numberOfInversions][i] = positions.pop();
+                }
+                for(int j = i;j < size;j++){
+                    positionsY[numberOfInversions][j] = 2000;
+                    positionsX[numberOfInversions][j] = 2000;
+                }
             }
+            numberOfInversions++;
             p.reset();
         }
+        fillRectangle(g, -220, -80, 100, 50, BG_2_COLOR);
+        fillRectangle(g, 150, -80, 100, 50, BG_2_COLOR);
         fillRectangle(g, -200, -80, 100, 50, BG_2_COLOR);
         for(int i = 0;i < level.length;i+=4) {
             fillRectangle(g, level[i], level[i+1], level[i+2], level[i+3], FG_2_COLOR);
         }
         g.setColor(FG_COLOR);
         g.fillRect(340, 265, 20, 20);
-        if(inverted) {
-            time--;
-            fillRectangle(g, -positionsX[time]-10, -positionsY[time]-10, 20, 20, FG_COLOR);
-        }else{
-            time++;
-            positions.push(p.getX());
-            positions.push(p.getY());
+
+        positions.push(p.getX());
+        positions.push(p.getY());
+
+        if(inverted) time--;
+        else time++;
+
+        for(int i = 0;i < numberOfInversions;i++){
+            try {
+                fillRectangle(g, -positionsX[i][time]-10, -positionsY[i][time]-10, 20, 20, FG_COLOR);
+            }catch(ArrayIndexOutOfBoundsException e){}
         }
+
         drawProgress(time, 600);
         try {
             long drawingTime = System.currentTimeMillis() - frameTime;
@@ -178,7 +206,7 @@ class Position {
     }
 
     public void reset(){
-        this.x = 0;
-        this.y = 0;
+        this.x = -200;
+        this.y = 60;
     }
 }
