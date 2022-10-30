3488704131efb438146638fad76f69614ce497f9
Keshuook <59391084+keshuook@users.noreply.github.com>
2022-10-30 15:21:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Label;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -31,7 +37,7 @@ public class Main {
             375, 350, 245, 120};
     private static Stack<Integer> positions = new Stack<>();
     private static boolean inverted = false;
-    private static int time = 300;
+    private static int time = 600;
     private static int FPS = 30;
     private static int numberOfInversions = 0;
     private static int[][] positionsX = new int[MAX_INV][];
@@ -39,17 +45,86 @@ public class Main {
     private static boolean[] pastBox = new boolean[MAX_INV];
     private static long frameTime = 0;
     private static int score = 0;
+    private static boolean playing = false;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         frame = new JFrame();
+
+        Label l = new Label();
+        l.setFont(FONT);
+        l.setText("Helo");
+        l.setForeground(FG_COLOR);
+
+        Button b = new Button();
+        b.setLabel("Play");
+        b.setFont(FONT);
+        b.setBounds(250, 230, 200, 50);
+
+        Button b2 = new Button();
+        b2.setLabel("?");
+        b2.setFont(FONT);
+        b2.setBounds(630, 0, 40, 40);
+
+        JLabel l2 = new JLabel();
+        l2.setBounds(0, 60, 700, 400);
+        l2.setText("<html><pre>In this game you will be able to travel through time in any direction (foward and backward).<br>Normally you travel fowards in time.<br>This means if you look at a clock, every second its hand will move by one second.<br>If you were to travel backwards, you would see the hand going anti-clockwise.<br>This game allows you to travel backwards in time and see your past selves.<br>This game was inspired by the movie \"Tenet\".<br><br>Your objective is to collect as many yellow coins as possible in any time (past or present).<br><br>RULES<ul><li> Use the arrow keys to move.</li><li> Collide with the yellow coins to collect them and increase your score.</li><li> Collide with the blue power-ups to invert the direction of time travel.<br> On collision, you will see a version of yourself from a different time.</li><li> Avoid colliding with your past self; it will cause you to lose.</li><li> Make sure you don't run out of time in either direction.</li></ul>Time and yourself are your enemies!<br>Good luck.</pre></html>");
+
+        l2.setForeground(FG_COLOR);
+        l2.hide();
+        frame.add(l2);
+
+        b.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    b.hide();
+                    b2.hide();
+                    playing = true;
+                }
+            });
+        b2.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(l2.isVisible()) {
+                        l2.hide();
+                        b.show();
+                        b2.setLabel("?");
+                    }else{
+                        l2.show();
+                        b.hide();
+                        b2.setLabel("X");
+                    }
+                }
+            });
+
+        frame.add(b);
+        frame.add(b2);
+        frame.setBackground(BG_COLOR);
+        frame.getContentPane().setBackground(BG_COLOR);
+        frame.setLayout(null);
 
         frame.setSize(700, 550);
         frame.setLocation(200, 50);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setBackground(BG_COLOR);
         frame.setVisible(true);
 
+        try {
+            while(true) {
+                if(!playing) {
+                    Thread.sleep(100);
+                    continue;
+                }
+                init();
+                break;
+            }
+        }
+        catch (Exception e) {
+            g.clearRect(0, 0, 700, 550);
+            l.setBounds(150, 230, 400, 50);
+            l.setText(e.getMessage());
+            frame.add(l);
+        }
+    }
+
+    private static void init() throws Exception {
         for(int i = 0;i < 20;i+=2) {
             coins[i] = new Coin((i*29)-265, -145);
             coins[i+1] = new Coin((i*29)-265, 235);
@@ -93,23 +168,25 @@ public class Main {
 
         if(inverted) time--;
         else time++;
+        
+        if(time > 1200 || time < 0) throw new Exception("You have run out of time");
 
         for(int i = 0;i < numberOfInversions;i++){
             try {
                 int x = positionsX[i][time],y = positionsY[i][time];
                 fillRectangle(g, x, y, 20, 20, FG_COLOR);
                 if(pastBox[i] && p.isTouchingSquare(x, y, 20)) {
-                    throw new Exception("Game over");   
+                    throw new Exception("You tried to interact with yourself.");   
                 }
                 if(!p.isTouchingSquare(x, y, 20)){
                     pastBox[i] = true;
                 }
             }catch(ArrayIndexOutOfBoundsException e){}
         }
-
+        
         handleCoins();
         handleScore();
-        drawProgress(time, 600);
+        drawProgress(time, 1200);
 
         try {
             long drawingTime = System.currentTimeMillis() - frameTime;
@@ -163,7 +240,6 @@ public class Main {
                     invertTime();
                     coins[i].setCollectedForever();
                 }else {
-                    System.out.println(coins[i].collected());
                     coins[i].setCollectedAt(time, inverted);
                     score+= 20;
                 }
